c8e2a6eff67c28f9de39aeea194ceafa8f5f67f7
zhouqungui <17370711222@163.com>
2023-03-02 21:43:36 +0800

    备份分支增加lombok依赖 -- by zqungui 230302

--- src/main/java/com/zqungui/write/model/UserEntity.java
+++ src/main/java/com/zqungui/write/model/UserEntity.java
@@ -1,26 +1,12 @@
 package com.zqungui.write.model;
 
+import lombok.Data;
 
+@Data
 public class UserEntity {
+
+    // 用户名
     private String userName;
+    // 密码
     private String password;
-
-    public UserEntity() {
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
